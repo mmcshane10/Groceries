9dbb95e83a6639bd6922e5b6a0458cfa6782f543
Student <example@example.com>
2019-06-12 20:43:11 -0700

    still trying to figure out js

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,26 @@
 $(document).ready(function() {
   $("form#blanks").submit(function(event) {
     event.preventDefault();
-
     var list = ["item1", "item2", "item3", "item4", "item5", "item6"];
     var groceries = list.map(function(input) {
       return $("input#" + input).val().toUpperCase();
     });
     var sortedGroceries = groceries.sort();
-debugger;
+    debugger;
 
-    Output.forEach(function(item) {
-      var userInput = $("input#" + item)
-      $("." + item).text(userInput)
+    // sortedGroceries.forEach(function(grocery) {
+    //   var userInput = $("input#" + grocery).val();
+    //   $("." + grocery).text(userInput);
+    //   console.log();
+    // });
 
-      $("form#blanks").hide();
-      console.log();
+    var html = "";
+    for (var i =0; i < list.length; i++) {
+      html += "<li>" + list[i]+ "</li>";
+    }
 
-    });
+    $("form#blanks").hide();
+    console.log();
+
   });
 });
